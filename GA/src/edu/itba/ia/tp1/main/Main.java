eefbc9ce17004db8434d6dc2b8e20283ec9a155c
mheras <mheras@d73e88a5-5e49-0410-8379-7944d79530be>
2008-03-28 17:25:07 +0000

    Cambie un poco el engine. Ahora no hay run, sino que hay step. Esto lo hice debido a que de alguna manera tenemos que poder detener el proceso. Esta es la forma mas facil. La otra seria hacer un nuevo thread para esto, pero me parecia demasiado...
    mato.

--- GA/src/edu/itba/ia/tp1/main/Main.java
+++ GA/src/edu/itba/ia/tp1/main/Main.java
@@ -2,6 +2,7 @@ package edu.itba.ia.tp1.main;
 
 import edu.itba.ia.tp1.engine.A_Problem;
 import edu.itba.ia.tp1.engine.Engine;
+import edu.itba.ia.tp1.engine.population.Population;
 import edu.itba.ia.tp1.engine.population.reproduction.ReproductionAlgorithm;
 import edu.itba.ia.tp1.engine.population.selection.EliteImpl;
 import edu.itba.ia.tp1.problem.binary2bcd.AptitudeImpl;
@@ -15,11 +16,21 @@ import edu.itba.ia.tp1.problem.binary2bcd.Problem;
 public class Main {
 
 	public static void main(String args[]) {
-		Engine engine = new Engine(200L, 20L);
-		A_Problem bin2Bcd = new Problem(new EliteImpl(),
-				new EliteImpl(), new ReproductionAlgorithm(),
-				new AptitudeImpl());
-		
-		engine.run(bin2Bcd);
+
+		A_Problem bin2Bcd = new Problem(new EliteImpl(), new EliteImpl(),
+				new ReproductionAlgorithm(), new AptitudeImpl());
+		Engine engine = new Engine(bin2Bcd, 200L, 20L);
+
+		/* Temporal annotation. */
+		@SuppressWarnings("unused")
+		Population currentPopulation;
+
+		while (engine.step()) {
+			currentPopulation = engine.getPopulation();
+			/*
+			 * Do something with this population (i.e. make statistics to show
+			 * them in a chart).
+			 */
+		}
 	}
 }
