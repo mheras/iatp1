262440fd083dad7980beec47c354f7be955bdd44
fsiviero <fsiviero@d73e88a5-5e49-0410-8379-7944d79530be>
2008-03-28 02:37:59 +0000

    Implementation of Elite

--- trunk/GA/src/edu/itba/ia/tp1/engine/population/selection/EliteImpl.java
+++ trunk/GA/src/edu/itba/ia/tp1/engine/population/selection/EliteImpl.java
@@ -3,8 +3,13 @@
  */
 package edu.itba.ia.tp1.engine.population.selection;
 
+import java.util.Collections;
+import java.util.List;
+
 import edu.itba.ia.tp1.engine.I_Aptitude;
+import edu.itba.ia.tp1.engine.population.A_Individual;
 import edu.itba.ia.tp1.engine.population.Population;
+import edu.itba.ia.tp1.engine.population.PopulationSortComparatorImpl;
 
 /**
  * Implementation of Elite Algorithm.
@@ -18,9 +23,21 @@ public class EliteImpl implements I_SelectionAlgorithm {
 	 * 
 	 * @see edu.itba.ia.tp1.motor.population.IPopulationAlgorithm#execute()
 	 */
+	/**
+	 * Sorts the population by descending order by the aptitude function and
+	 * returns a new Population with these Individuals.
+	 */
 	@Override
-	public Population execute(Population population, I_Aptitude aptitude,Integer nIndividuals) {
-		return null;
-	}
+	public Population execute(Population population, I_Aptitude aptitude,
+			Long nIndividuals) {
+		Collections.sort(population.getIndividuals(), new PopulationSortComparatorImpl(aptitude));
+		
+		List<A_Individual> sorted = population.getIndividuals();
+		List<A_Individual> elite = sorted.subList(0, nIndividuals.intValue());
+		
+		Population newPopulation = new Population();
+		newPopulation.addAll(elite);
 
+		return newPopulation;
+	}
 }
