6ec8dd7d42a98e492ebad07ceec75c8dc485c88e
fsiviero <fsiviero@d73e88a5-5e49-0410-8379-7944d79530be>
2008-04-06 18:58:34 +0000

    Implemented Roulette.

--- GA/src/edu/itba/ia/tp1/engine/population/selection/RouletteImpl.java
+++ GA/src/edu/itba/ia/tp1/engine/population/selection/RouletteImpl.java
@@ -1,6 +1,10 @@
 package edu.itba.ia.tp1.engine.population.selection;
 
+import java.util.List;
+import java.util.Random;
+
 import edu.itba.ia.tp1.engine.population.Population;
+import edu.itba.ia.tp1.engine.population.Utils;
 
 /**
  * Implementation of Roulette Algorithm
@@ -15,7 +19,23 @@ public class RouletteImpl implements I_SelectionAlgorithm {
 	 * @see edu.itba.ia.tp1.motor.population.IPopulationAlgorithm#execute()
 	 */
 	public Population execute(Population population, Long nIndividuals) {
-		return null;
-	}
+		Random random = new Random();
+		Population ret = new Population();
+		List<Double> cumulativeFrequencies = Utils.getCumulativeFrequencies(population);
+		
+		for (int i = 0; i < nIndividuals; i++) {
+			Double randomNumber = random.nextDouble();
+			for (int j = 1; j < population.getSize(); j++) {
+				Double cumFreq1 = cumulativeFrequencies.get(j - 1);
+				Double cumFreq2 = cumulativeFrequencies.get(j);
+				Boolean capable = ((cumFreq1 < randomNumber) &&
+								   (randomNumber < cumFreq2));
+				if (capable) {
+					ret.addIndividual(population.getIndividualByPosition(i));
+				}
+			} 
+		}
 
+		return ret;
+	}
 }
